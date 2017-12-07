15701f82511d1736c880b296eb6878cbf367c35a
Megatron <1demidz3@gmail.com>
2017-12-07 23:31:29 +0300

    Add project files.

---
+++ FactoryExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factory Test");
+            PersonFactory P = new PersonFactory();
+            P.AddPeople("Jane");
+            P.AddPeople("Jack");
+            P.AddPeople("Jeff");
+            P.AddPeople("Keith");
+            P.AddPeople("Mary");
+            foreach(person k in P.People)
+            {
+                Console.WriteLine("Name: " + k.Name + " Id " + k.Id.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FactoryExercise/person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryExercise
+{
+    
+    public class person
+    {
+/       public int Id { get; set; }
+        public string Name { get; set; }
+
+        
+    }
+
+    public class PersonFactory
+    {
+        public List<person> People = new List<person>();
+        private person CreatePerson(string Name) {
+            person P = new person();
+            P.Name = Name;
+            return P;
+        }
+
+        //альтернативно можно вставить функционал addpeople в createperson. Но это тогда 2 дела за раз
+
+        public void AddPeople(string Name)
+        {
+
+            People.Add(CreatePerson(Name));
+            People[People.Count - 1].Id = People.Count - 1;
+
+
+        }
+
+    }
+
+    
+
+
+}
